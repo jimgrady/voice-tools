591bdb899c446e9f6b8ed9eaef1a811d1b6d809a
Jim Grady <jim@stopbreathethink.com>
2022-10-30 22:42:05 -0400

    refactor template tagging system

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,17 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'standard'
+    'standard',
+    'eslint:recommended'
   ],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12
   },
   rules: {
+    semi: [2, 'never'],
+    'no-implicit-globals': ['error'],
+    'no-undef': ['error'],
+    'no-restricted-globals': ['error', 'self']
   }
 }
